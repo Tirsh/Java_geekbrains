a3c6adef94f4d0652a6fc73f7fc18154fa0649f7
Tirsh <valeriy.vasipov@gmail.com>
2022-11-26 14:00:37 +0300

    Hemowork2-2

--- src/homework2/Task1.java
+++ src/homework2/Task1.java
@@ -1,89 +1,26 @@
 package homework2;
-
-
-import java.util.Map;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Task1 {
     public static void main(String[] args) {
-        String jsonStr = String.format("{\n" +
-                "  \"firstName\": \"John\",\n" +
-                "  \"lastName\": \"Smith\",\n" +
-                "  \"isAlive\": true,\n" +
-                "  \"age\": 27,\n" +
-                "  \"address\": {\n" +
-                "    \"streetAddress\": \"21 2nd Street\",\n" +
-                "    \"city\": \"New York\",\n" +
-                "    \"state\": \"NY\",\n" +
-                "    \"postalCode\": \"10021-3100\"\n" +
-                "  },\n" +
-                "  \"phoneNumbers\": [\n" +
-                "    {\n" +
-                "      \"type\": \"home\",\n" +
-                "      \"number\": \"212 555-1234\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"type\": \"office\",\n" +
-                "      \"number\": \"646 555-4567\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"children\": [\n" +
-                "      \"Catherine\",\n" +
-                "      \"Thomas\",\n" +
-                "      \"Trevor\"\n" +
-                "  ],\n" +
-                "  \"spouse\": null\n" +
-                "}");
-        System.out.println(jsonStr);
-        //MyJson myJson = new MyJson(jsonStr);
-
+        String jsonStr = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        System.out.println(createRequest(jsonStr));
     }
-
-
-
-
+    public static String createRequest(String jsonStr){
+        StringBuilder result = new StringBuilder("select * from students where ");
+        Pattern pattern = Pattern.compile("\"\\w+\":\"\\w+\"");
+        Matcher matcher = pattern.matcher(jsonStr);
+        String[] strArr = new String[2];
+        String and = "";
+        while (matcher.find()){
+            if (strArr[0] != null) and = " AND ";
+            strArr = jsonStr.substring(matcher.start()+1, matcher.end()-1).split("\":\"");
+            if (!strArr[1].equals("null")) {
+                result.append(String.format(and));
+                result.append(String.format("%s = '%s'", strArr[0], strArr[1]));
+            }
+        }
+        result.append(";");
+        return result.toString();
+    }
 }
-
-class MyJson{
-    private Map<String,String[]> jsonMap;
-
-    public MyJson(String jsonStr) {
-        this.jsonMap = jsonMap;
-        jsonStrToMap(jsonStr);
-    }
-    private void jsonStrToMap(String str){
-        String json = str.replaceAll("\\s+", "");
-        //System.out.println(json.replaceAll("\"\\w+\":\\{[\":\\-,\\w]+\\}", ""));
-        System.out.println(json.replaceAll("\"\\w+\":\"[\\w-]+\"|\"\\w+\":\\w+", "'':''"));
-    }
-
-    private
-}
-
-//{
-//        "firstName": "John",
-//        "lastName": "Smith",
-//        "isAlive": true,
-//        "age": 27,
-//        "address": {
-//        "streetAddress": "21 2nd Street",
-//        "city": "New York",
-//        "state": "NY",
-//        "postalCode": "10021-3100"
-//        },
-//        "phoneNumbers": [
-//        {
-//        "type": "home",
-//        "number": "212 555-1234"
-//        },
-//        {
-//        "type": "office",
-//        "number": "646 555-4567"
-//        }
-//        ],
-//        "children": [
-//        "Catherine",
-//        "Thomas",
-//        "Trevor"
-//        ],
-//        "spouse": null
-//        }
---
+++ src/homework2/Task2.java
@@ -0,0 +1,39 @@
+package homework2;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.*;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 2, 18, 6};
+
+        try {
+            System.out.println(Arrays.toString(bubbleSort(arr)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static int[] bubbleSort(int[] array) throws IOException {
+        Logger logger = Logger.getLogger(Task2.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+        boolean sorted = false;
+        int tmp;
+        while (!sorted){
+            for (int i = 0; i < array.length-1; i++) {
+                sorted = true;
+                if (array[i] > array[i+1]){
+                    sorted = false;
+                    tmp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = tmp;
+                }
+                logger.log(Level.INFO, Arrays.toString(array));
+            }
+        }
+        return array;
+    }
+}
