2474821a8a24c9f5d542e5b7c53573eab109fbe2
Tirsh <valeriy.vasipov@gmail.com>
2022-12-06 23:28:48 +0300

    homework5

---
+++ src/homework5/Task1.java
@@ -0,0 +1,39 @@
+package homework5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task1 {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addPhone("Alex", "+79819841752");
+        phoneBook.addPhone("Alex", "+79817325313");
+        phoneBook.addPhone("Bob", "+79212320990");
+        phoneBook.addPhone("Ann", "+79030934002");
+        System.out.println(phoneBook.getPhone("Bob"));
+        phoneBook.showAll();
+    }
+}
+class PhoneBook{
+    Map<String, ArrayList<String>> data;
+
+    public PhoneBook() {
+        data = new HashMap<>();
+    }
+    public void addPhone(String name, String number){
+        if (data.containsKey(name))
+            data.get(name).add(number);
+        else
+            data.put(name, new ArrayList<>(List.of(number)));
+    }
+    public ArrayList<String> getPhone(String name){
+        return data.get(name);
+    }
+    public void showAll(){
+        for(var item: data.entrySet()){
+            System.out.println(item.getKey() + " : " + item.getValue());
+        }
+    }
+}
---
+++ src/homework5/Task2.java
@@ -0,0 +1,36 @@
+package homework5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<String> employees = new ArrayList<>();
+        employees.add("Иван Иванов");
+        employees.add("Светлана Петрова");
+        employees.add("Кристина Белова");
+        employees.add("Анна Мусина");
+        employees.add("Анна Крутова");
+        employees.add("Иван Юрин");
+        employees.add("Петр Лыков");
+        employees.add("Павел Чернов");
+        employees.add("Петр Чернышов");
+        employees.add("Мария Федорова");
+        employees.add("Марина Светлова");
+        employees.add("Мария Савина");
+        employees.add("Мария Рыкова");
+        employees.add("Марина Лугова");
+        employees.add("Анна Владимирова");
+        employees.add("Иван Мечников");
+        employees.add("Петр Петин");
+        employees.add("Иван Ежов");
+        employees.stream().map(name -> name.split("\\s")[0])
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(System.out::println);
+    }
+}
