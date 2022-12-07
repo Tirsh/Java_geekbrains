ce8d39ede18fcafb70713fec881ce07de0367d06
Tirsh <valeriy.vasipov@gmail.com>
2022-12-07 20:28:22 +0300

    h2

--- src/homework5/Task3.java
+++ src/homework5/Task3.java
@@ -2,3 +2,12 @@ package homework5;
 
 public class Task3 {
 }
+class HeapSort{
+    public static int[] sort(int[] arr){
+        int arrayLength = arr.length - 1;
+        for (int i = arrayLength; i > 0; i --){
+
+        }
+        return arr;
+    }
+}
