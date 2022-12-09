374c987b5a8e55d62695c89f196c91a5284f70c4
Valeriy <valeriy.vasipov@gmail.com>
2022-12-09 14:18:00 +0300

    homework5 - 4

--- src/homework5/Task4.java
+++ src/homework5/Task4.java
@@ -1,4 +1,38 @@
 package homework5;
 
+import java.util.Arrays;
+
 public class Task4 {
+    public static void main(String[] args) {
+
+    }
+}
+class ChessBoard{
+    private byte[][] board;
+    private int numberOfQueens;
+    private int boardSize;
+
+
+    public ChessBoard(int size) {
+        board = new byte[size][size];
+        boardSize = size;
+        numberOfQueens = 0;
+    }
+
+    public ChessBoard(ChessBoard board) {
+        this.board = board.board;
+        this.numberOfQueens = board.numberOfQueens;
+        this.boardSize = board.boardSize;
+    }
+    public boolean putQueen(int x, int y){
+        if (board[x][y] != 0x0){
+            return false;
+        }
+        // put queen and mack board
+        return true;
+    }
+    public boolean checkQueensOnBoard(int number){
+        if (numberOfQueens == number) return true;
+        else return false;
+    }
 }
