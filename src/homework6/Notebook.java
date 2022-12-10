f3cb1c420a2993d4037bd77a64633bd6d7138dd2
Tirsh <valeriy.vasipov@gmail.com>
2022-12-10 11:58:47 +0300

    homework6-0

---
+++ src/homework6/Notebook.java
@@ -0,0 +1,60 @@
+package homework6;
+
+public class Notebook {
+    private final int ram;
+    private final int hdVolume;
+    private final String operationSystem;
+    private final String brand;
+    private final String model;
+    private final Cpu cpu;
+    private final String color;
+
+    public Notebook(String brand, String model, Cpu cpu, int ram, int hdVolume, String operationSystem, String color) {
+        this.ram = ram;
+        this.hdVolume = hdVolume;
+        this.operationSystem = operationSystem;
+        this.brand = brand;
+        this.model = model;
+        this.cpu = cpu;
+        this.color = color;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdVolume() {
+        return hdVolume;
+    }
+
+    public String getOperationSystem() {
+        return operationSystem;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Cpu getCpu() {
+        return cpu;
+    }
+
+    @Override
+    public String toString() {
+        return "Notebook{" +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", cpu=" + cpu +
+                "ram=" + ram +
+                ", hdVolume=" + hdVolume +
+                ", operationSystem='" + operationSystem + '\'' +
+                '}';
+    }
+}
+enum Cpu{
+    AMD , INTEL
+}
---
+++ src/homework6/NotebookStore.java
@@ -0,0 +1,39 @@
+package homework6;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NotebookStore {
+    Set<Notebook> notebooksOnStock;
+
+    public NotebookStore() {
+        notebooksOnStock = new HashSet<>();
+    }
+
+    public NotebookStore(Notebook[] notebooks) {
+        notebooksOnStock = new HashSet<>(Arrays.asList(notebooks));
+    }
+    public Set<Notebook> findByOptions(Notebook protoNote){
+
+        return notebooksOnStock;
+    }
+    private Notebook createRequest(){
+        int ram;
+        int hdVolume;
+        String os;
+        String color;
+        int option = 0;
+        while(option != 5) {
+            System.out.println("   “Введите цифру, соответствующую необходимому критерию:  \n" +
+                    "   1 - ОЗУ  \n" +
+                    "   2 - Объем ЖД  \n" +
+                    "   3 - Операционная система  \n" +
+                    "   4 - Цвет  \n" +
+                    "   5 - Поиск");
+        }
+
+        return new Notebook("","", null, 0, 0, "", "");
+    }
+
+}
